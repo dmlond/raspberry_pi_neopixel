6de40f8040624720ff11eb430d8bb9e7502718dd
Darin London <darin.london@duke.edu>
2020-12-31 22:43:35 -0500

    takes an array of colors, and dequeues it for self.colors uses len(colors) to mod each pixel

--- lib/neopixc.py
+++ lib/neopixc.py
@@ -3,14 +3,10 @@ from neopixel import NeoPixel
 from collections import deque
 
 class NeoPixC(NeoPixel):
-  def __init__(self):
+  def __init__(self, colors):
     self.num_pixels = 100
     self.pin = board.D18
-    self.color_mod = deque([
-      self.green,
-      self.red,
-      self.yellow
-    ])
+    self.colors = deque(colors)
 
     super().__init__(
       self.pin,
@@ -19,25 +15,16 @@ class NeoPixC(NeoPixel):
       auto_write=False
     )
   
-  def green(self,pixel):
-    self[pixel] = (255,0,0)
-
-  def red(self,pixel):
-    self[pixel] = (0,255,0)
-
-  def yellow(self,pixel):
-    self[pixel] = (255,255,0)
-
   def walk(self):
     curr_pixel = 0
     while curr_pixel < self.num_pixels:
-        mod = curr_pixel % 3
-        self.color_mod[mod](curr_pixel)
+        mod = curr_pixel % len(self.colors)
+        self[curr_pixel] = self.colors[mod]
         curr_pixel += 1
     self.show()
 
   def rotate(self):
-    self.color_mod.rotate()
+    self.colors.rotate()
 
   def down(self):
     self.fill((0,0,0))
