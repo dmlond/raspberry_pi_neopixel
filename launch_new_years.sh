b295a4ed4a6115536570c199e04ec536b085cff9
Darin London <darin.london@duke.edu>
2021-01-02 10:48:01 -0500

    added launch_new_years.sh to launch the new_years.json colors modified both launch scripts to create pid files

--- launch_new_years.sh
+++ launch_new_years.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp christmas.json colors
+cp new_years.json colors
 sudo python3 /home/pi/run_lights.py > /home/pi/run_lights.log 2>&1 &
 ps aux | grep run_lights | head -1 | awk '{print $2}' > /home/pi/run_lights.pid
 export FLASK_APP=/home/pi/neopix_controller
